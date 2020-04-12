52e8cb8048bace347661d042377a21656c8ae27b
bsheenml <brendan.sheen@gmail.com>
2020-04-12 06:03:03 -0700

    Add Application Component

---
+++ src/components/Application.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { Router } from "@reach/router";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import UserProvider from "../providers/UserProvider";
+import ProfilePage from "./ProfilePage";
+import { UserContext } from "../providers/UserProvider";
+import PasswordReset from "./PasswordReset";
+function Application() {
+  const user = useContext(UserContext);
+  return (
+        user ?
+        <ProfilePage />
+      :
+        <Router>
+          <SignUp path="signUp" />
+          <SignIn path="/" />
+          <PasswordReset path = "passwordReset" />
+        </Router>
+      
+  );
+}
+
+export default Application;
